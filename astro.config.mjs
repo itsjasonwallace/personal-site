6183629deda1e84654a8ad577e4f4726485b8713
Jason <JasonW.mn@gmail.com>
2026-05-27 13:54:42 -0600

    version bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://itsjasonwallace.com",
   output: "static",
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
